3a1397f136a6c669a6d736a8c8b967a36738f0bc
AliKlein <alexandraeklein@yahoo.com>
2020-12-09 11:47:24 -0800

    add react helmet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import { Helmet } from "react-helmet";
+import Head from "./components/Head";
 import Map from "./components/Map";
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Helmet>
+          <Head />
+        </Helmet>
         <Map />
       </div>
     );
---
+++ src/components/Head/index.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const Head = () => (
+  <Fragment>
+    <meta charSet="utf-8" />
+    <title>Are Salons Open</title>
+    <link rel="canonical" href="http://aresalonsopen.com" />
+  </Fragment>
+);
+
+export default Head;
