edf900ae368da8a7ade845c9b113db37aeddad73
AliKlein <alexandraeklein@yahoo.com>
2020-12-08 17:00:33 -0800

    first commits

--- src/App.js
+++ src/App.js
@@ -1,44 +1,94 @@
 import React, { Component } from "react";
-import MapGL, { Marker } from "react-map-gl";
-import glamorous from "glamorous";
+import ReactMapGL, { Source, Layer } from "react-map-gl";
+import styled from "@emotion/styled";
+import geojson from "./geojson.json";
+
 require("dotenv").config();
 
-const POI = glamorous.div({
-  borderRadius: "50px",
-  width: "50px",
-  height: "50px",
-  backgroundColor: "red"
-});
+const StyledMap = styled.div`
+  .mapgl-mapbox {
+    min-height: 70vh;
+    min-width: 100vw;
+  }
+`;
 
 class App extends Component {
   state = {
     viewport: {
-      width: 1000,
-      height: 500,
+      width: 1200,
+      height: 1000,
       latitude: 37.7577,
       longitude: -122.4376,
-      zoom: 8
-    }
+      zoom: 8,
+    },
+  };
+
+  onMapClick = (event) => {
+    const {
+      features,
+      srcEvent: { offsetX, offsetY },
+    } = event;
+
+    const feature = features.find(
+      (feature) => feature.layer.id === "fill-layer"
+    );
+
+    console.log(feature);
+  };
+
+  onMapLoaded = (map) => {
+    this.map = map;
+    console.log({ map });
   };
 
   render() {
     return (
       <div className="App">
-        <MapGL
-          {...this.state.viewport}
-          mapStyle="mapbox://styles/khtin/cjk46q8hh4fen2sqz98wylqrd"
-          mapboxApiAccessToken={process.env.REACT_APP_MapboxAccessToken}
-          onViewportChange={viewport => this.setState({ viewport })}
-        >
-          <Marker
-            latitude={37.78}
-            longitude={-122.41}
-            offsetLeft={-20}
-            offsetTop={-10}
+        <StyledMap>
+          <ReactMapGL
+            {...this.state.viewport}
+            data={geojson}
+            mapStyle="mapbox://styles/mbxsolutions/ck4ye87f3nlti1co2al1wpsnz"
+            mapboxApiAccessToken={process.env.REACT_APP_MapboxAccessToken}
+            onViewportChange={(viewport) => this.setState({ viewport })}
+            onClick={this.onMapClick}
+            onLoad={this.onMapLoaded}
+            className="mapgl-mapbox"
           >
-            <POI>TRENDDDING!!!🔥🔥🔥🔥🔥🔥🔥🔥</POI>
-          </Marker>
-        </MapGL>
+            <Source id="my-data" type="geojson" data={geojson}>
+              <Layer
+                id="fill-layer"
+                type="fill"
+                paint={{
+                  "fill-opacity": 0.5,
+                  "fill-color": [
+                    "interpolate",
+                    ["linear"],
+                    ["get", "unemployment_rate"],
+                    0,
+                    "#F2F12D",
+                    1,
+                    "#EED322",
+                    2,
+                    "#E6B71E",
+                    3,
+                    "#DA9C20",
+                    4,
+                    "#CA8323",
+                    5,
+                    "#B86B25",
+                    6,
+                    "#A25626",
+                    7,
+                    "#8B4225",
+                    8,
+                    "#723122",
+                  ],
+                }}
+              />
+            </Source>
+          </ReactMapGL>
+        </StyledMap>
       </div>
     );
   }
